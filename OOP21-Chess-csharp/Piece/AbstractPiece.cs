5925fff835d66b32d2b76065c9cbb877130df6e3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 18:25:33 +0200

    Create AbstractPiece.cs

---
+++ OOP21-Chess-csharp/Piece/AbstractPiece.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using OOP21_Chess_csharp.Utils;
+
+namespace OOP21_Chess_csharp.Piece
+{
+    public abstract class AbstractPiece : IPiece
+    {
+        private readonly bool _isMoved;
+        protected Directions Directions { get; }
+        public Name Name { get; }
+        public Side Side { get; }
+        public Position Position { get; private set; }
+        protected AbstractPiece(Name name, Position position, Side side)
+        {
+            Name = name;
+            Position = position;
+            Side = side;
+            _isMoved = false;
+            Directions = new Directions();
+        }
+        
+        public void SetPosition(Position position) => Position = position;
+
+        public bool IsMoved() => _isMoved;
+
+        public abstract List<Position> GetAllPossiblePositions { get; }
+
+        public abstract int Value { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AbstractPiece piece &&
+                   Name == piece.Name &&
+                   Side == piece.Side;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1530612366;
+            hashCode = hashCode * -1521134295 + Name.GetHashCode();
+            hashCode = hashCode * -1521134295 + Side.GetHashCode();
+            return hashCode;
+        }
+    }
+}
