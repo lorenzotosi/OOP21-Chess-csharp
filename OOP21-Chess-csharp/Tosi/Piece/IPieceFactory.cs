30360e2f6781c65fe8706554fc9d232a47e74c13
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 19:03:55 +0200

    add interface

--- OOP21-Chess-csharp/Tosi/Piece/IPieceFactory.cs
+++ OOP21-Chess-csharp/Tosi/Piece/IPieceFactory.cs
@@ -1,7 +1,9 @@
+using OOP21_Chess_csharp.Tosi.Utils;
+
 namespace OOP21_Chess_csharp.Tosi.Piece
 {
     public interface IPieceFactory
     {
-        
+        IPiece CreatePiece(Name name, Position position, Side color);
     }
 }
