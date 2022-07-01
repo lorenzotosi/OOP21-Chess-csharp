dd08041ffcafb760e0ccedcb88425985306ee446
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 18:25:31 +0200

    Create IPiece.cs

---
+++ OOP21-Chess-csharp/Piece/IPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using OOP21_Chess_csharp.Utils;
+
+namespace OOP21_Chess_csharp.Piece
+{
+    public interface IPiece
+    {
+        Name Name { get; }
+
+        void SetPosition(Position position);
+
+        Side Side { get; }
+
+        int Value { get; }
+
+        bool IsMoved();
+
+        Position Position { get; }
+
+        List<Position> GetAllPossiblePositions { get; }
+    }
+}
