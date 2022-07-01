36039e79fd497c5d796a01e12eba6a4a2dd2415b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 21:17:00 +0200

    quick update

--- OOP21-Chess-csharp/Program.cs
+++ OOP21-Chess-csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using OOP21_Chess_csharp.Tosi.Piece;
+using OOP21_Chess_csharp.Tosi.Utils;
 
 namespace OOP21_Chess_csharp
 {
@@ -7,6 +9,12 @@ namespace OOP21_Chess_csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var factory = new PieceFactory();
+            var queen = factory.CreatePiece(Name.Queen, Position.CreateNewPosition("a1"), Side.Black);
+            foreach (var position in queen.GetAllPossiblePositions)
+            {
+                Console.WriteLine(position);
+            }
         }
     }
 }
