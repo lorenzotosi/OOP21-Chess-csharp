8045d44305c95e5a1c468bbbf8fccb9132b7dd49
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 18:17:05 +0200

    first commit

---
+++ OOP21-Chess-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP21_Chess_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
