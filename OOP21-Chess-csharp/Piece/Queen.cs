e6df39bbf4d84537fc305c8bc135ebadd866b2cd
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-01 18:25:24 +0200

    Create Queen.cs

---
+++ OOP21-Chess-csharp/Piece/Queen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using OOP21_Chess_csharp.Move;
+using OOP21_Chess_csharp.Utils;
+
+namespace OOP21_Chess_csharp.Piece
+{
+    public class Queen : AbstractPiece
+    {
+        private const int QueenValue = 9;
+        protected Queen(Position position, Side side) : base(Name.Queen, position: position, side: side)
+        {
+        }
+
+        public override List<Position> GetAllPossiblePositions => new PieceMovementImpl()
+            .MultipleMove(Directions.QueenDir(), this);
+
+        public override int Value => QueenValue;
+    }
+}
