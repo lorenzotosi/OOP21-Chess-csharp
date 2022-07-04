6a44da0532a5528d8ab9dbc1394ec7cdd5eef727
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-07-04 22:27:32 +0200

    create Chessboard class and interface

---
+++ OOP21-Chess-csharp/MarcoRaggini/Chessboard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OOP21_Chess_csharp.Tosi.Piece;
+using OOP21_Chess_csharp.Tosi.Utils;
+
+namespace OOP21_Chess_csharp.MarcoRaggini
+{
+    public class Chessboard:IChessboard
+    {
+        public List<IPiece> PiecesList { get; }
+        public int XBorder { get; }
+        public int YBorder { get; }
+
+        public Chessboard(List<IPiece> piecesList, int xborder, int yborder)
+        {
+            this.PiecesList = piecesList;
+            this.XBorder = xborder;
+            this.YBorder = yborder;
+        }
+
+        public void Move(Position actualPos, Position finalPos)
+        {
+            var attacker = (GetPieceOnPosition(actualPos));
+            if (CanKill(finalPos))
+            {
+                PiecesList.Remove(GetPieceOnPosition(finalPos));
+            }
+            attacker?.SetPosition(finalPos);
+        }
+        public IPiece GetPieceOnPosition(Position selectedPos)
+        {
+            return PiecesList.FirstOrDefault(t => t.Position.Equals(selectedPos));
+        }
+
+        private bool CanKill(Position targetPos)
+        {
+            return GetPieceOnPosition(targetPos) is not null;
+        }
+
+        public override string ToString()
+        {
+            return $"Chessboard = {PiecesList.Select(x => x.ToString())}";
+        }
+    }
+}
---
+++ OOP21-Chess-csharp/MarcoRaggini/IChessboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using OOP21_Chess_csharp.Tosi.Piece;
+using OOP21_Chess_csharp.Tosi.Utils;
+
+namespace OOP21_Chess_csharp.MarcoRaggini
+{
+    public interface IChessboard
+    {
+        public List<IPiece> PiecesList { get; }
+        public int XBorder { get; }
+        public int YBorder { get; }
+        public void Move(Position actualPos, Position finalPos);
+        public IPiece GetPieceOnPosition(Position selectedPos);
+    }
+}
