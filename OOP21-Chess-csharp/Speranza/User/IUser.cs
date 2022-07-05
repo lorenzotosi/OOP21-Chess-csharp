e709717f799ab4149fbedf24ccd31a97d24874c2
Angela Speranza <angela.speranza@studio.unibo.it>
2022-07-05 21:43:22 +0200

    implementation of the User's class + interface

---
+++ OOP21-Chess-csharp/Speranza/User/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace OOP21_Chess_csharp.Speranza.User
+{
+    public interface IUser
+    { 
+        string Name { get; }
+        void HaveWon();
+        bool IsGameWinner { get; }
+    }
+}
---
+++ OOP21-Chess-csharp/Speranza/User/User.cs
@@ -0,0 +1,45 @@
+﻿namespace OOP21_Chess_csharp.Speranza.User
+{
+    public class User : IUser
+    {
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public void HaveWon()
+        {
+            IsGameWinner = true;
+        }
+
+        public bool IsGameWinner { get; private set; }
+        
+        public override string ToString() {
+            return Name;
+        }
+        
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((User)obj);
+        }
+    }
+}
