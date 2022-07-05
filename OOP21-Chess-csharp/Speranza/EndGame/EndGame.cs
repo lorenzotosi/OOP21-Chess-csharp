5f964a42620d99f1402491f8425e0f6da89fcb5e
Angela Speranza <angela.speranza@studio.unibo.it>
2022-07-05 21:43:48 +0200

    implementation of the EndGame's class + interface

---
+++ OOP21-Chess-csharp/Speranza/EndGame/EndGame.cs
@@ -0,0 +1,28 @@
+﻿using OOP21_Chess_csharp.MarcoRaggini;
+using OOP21_Chess_csharp.Tosi.Utils;
+
+namespace OOP21_Chess_csharp.Speranza.EndGame
+{
+    public class EndGame : IEndGame
+    {
+        public bool IsCheckmate(IChessboard chessboard, Side side)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsStalemate(IChessboard chessboard, Side side)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsDrawByInsufficientMaterial(IChessboard chessboard)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsDraw(IChessboard chessboard, Side side)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OOP21-Chess-csharp/Speranza/EndGame/IEndGame.cs
@@ -0,0 +1,16 @@
+﻿using OOP21_Chess_csharp.MarcoRaggini;
+using OOP21_Chess_csharp.Tosi.Utils;
+
+namespace OOP21_Chess_csharp.Speranza.EndGame
+{
+    public interface IEndGame
+    {
+        bool IsCheckmate(IChessboard chessboard, Side side);
+
+        bool IsStalemate(IChessboard chessboard, Side side);
+
+        bool IsDrawByInsufficientMaterial(IChessboard chessboard);
+
+        bool IsDraw(IChessboard chessboard, Side side);
+    }
+}
