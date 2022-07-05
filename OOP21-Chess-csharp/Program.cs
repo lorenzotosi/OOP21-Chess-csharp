cea6e4170cc5b595012a5943aee7314403898198
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-07-05 22:56:41 +0200

    test

--- OOP21-Chess-csharp/Program.cs
+++ OOP21-Chess-csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using OOP21_Chess_csharp.MarcoRaggini;
 using OOP21_Chess_csharp.Tosi.Piece;
 using OOP21_Chess_csharp.Tosi.Utils;
 
@@ -10,11 +12,20 @@ namespace OOP21_Chess_csharp
         {
             Console.WriteLine("Hello World!");
             var factory = new PieceFactory();
+            var l = new List<IPiece>
+            {
+                factory.CreatePiece(Name.Knight, Position.CreateNewPosition("a2"), Side.White),
+                factory.CreatePiece(Name.Knight, Position.CreateNewPosition("b2"), Side.White),
+                factory.CreatePiece(Name.Knight, Position.CreateNewPosition("b1"), Side.Black)
+            };
+            var board = new Chessboard(l, 7, 7);
             var queen = factory.CreatePiece(Name.Queen, Position.CreateNewPosition("a1"), Side.Black);
-            foreach (var position in queen.GetAllPossiblePositions)
+            foreach (var position in queen.GetAllPossiblePositions(board))
             {
                 Console.WriteLine(position);
             }
+            Console.WriteLine(ControlUtils.CheckEnemyOnPosition(Position.CreateNewPosition("a3"), board, queen));
+            Console.WriteLine(ControlUtils.CheckPieceOnPosition(Position.CreateNewPosition("a1"), board));
         }
     }
 }
