4034de0e0f7e71a462015af2846c2502c4c530e0
Angela Speranza <angela.speranza@studio.unibo.it>
2022-07-05 21:44:11 +0200

    implementation of Castling's class + interface

---
+++ OOP21-Chess-csharp/Speranza/Castling/Castling.cs
@@ -0,0 +1,13 @@
+﻿using OOP21_Chess_csharp.MarcoRaggini;
+using OOP21_Chess_csharp.Tosi.Piece;
+
+namespace OOP21_Chess_csharp.Speranza.Castling
+{
+    public class Castling : ICastling
+    {
+        public bool CanCastle(IChessboard chessboard, IPiece king, int xRook)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OOP21-Chess-csharp/Speranza/Castling/ICastling.cs
@@ -0,0 +1,10 @@
+﻿using OOP21_Chess_csharp.MarcoRaggini;
+using OOP21_Chess_csharp.Tosi.Piece;
+
+namespace OOP21_Chess_csharp.Speranza.Castling
+{
+    public interface ICastling
+    {
+        bool CanCastle(IChessboard chessboard, IPiece king, int xRook);
+    }
+}
