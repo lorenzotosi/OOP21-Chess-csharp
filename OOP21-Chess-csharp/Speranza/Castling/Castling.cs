71d96aca2c10bbc3ec1fd06b9dfd394e9702e29c
Angela Speranza <angela.speranza@studio.unibo.it>
2022-07-05 23:28:01 +0200

    methods implementation

--- OOP21-Chess-csharp/Speranza/Castling/Castling.cs
+++ OOP21-Chess-csharp/Speranza/Castling/Castling.cs
@@ -1,5 +1,9 @@
-﻿using OOP21_Chess_csharp.MarcoRaggini;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OOP21_Chess_csharp.MarcoRaggini;
 using OOP21_Chess_csharp.Tosi.Piece;
+using OOP21_Chess_csharp.Tosi.Utils;
 
 namespace OOP21_Chess_csharp.Speranza.Castling
 {
@@ -7,7 +11,69 @@ namespace OOP21_Chess_csharp.Speranza.Castling
     {
         public bool CanCastle(IChessboard chessboard, IPiece king, int xRook)
         {
-            throw new System.NotImplementedException();
+            /* return chessboard.GetPieceOnPosition(Position.CreateNumericPosition(xRook, king.Position.Y)
+                .Select(r => IsCastlePossible(chessboard, king, r))
+                .DefaultIfEmpty(false)
+                .First(); */
+            return true;
+        }
+        
+        private bool IsPositionUnderAttack(IChessboard chessboard, Position position, Side attackedSide)
+        {
+            return chessboard.PiecesList.Where(x => !x.Side.Equals(attackedSide))
+                .Where(x => !x.Name.Equals(Name.King))
+                .Any(p => p.GetAllPossiblePositions(chessboard).Contains(position));
+        }
+        
+        private bool IsCastlePossible(IChessboard chessboard, IPiece king, IPiece rook)
+        {
+            // we're working with the correct pieces;
+            if (!king.Name.Equals(Name.King) || !rook.Name.Equals(Name.Rook))
+            {
+                return false;
+            }
+            // the pieces must not have been moved;
+            if (king.IsMoved() || rook.IsMoved())
+            {
+                return false;
+            }
+            if (KingInCheck.isInCheckWithoutKing(chessboard, king.Side))
+            {
+                return false;
+            }
+
+            var positions = HorizontalPositionsBetweenPieces(rook, king);
+
+            // the list must contain only non-occupied positions;
+            if (positions.Any(p => chessboard.PiecesList.Select(x => x.Position).Contains(p)))
+            {
+                return false;
+            }
+            
+            // filter the previous list and get the positions the king has to go through.
+            return positions.Where(p => Math.Abs(king.Position.X - p.X) <= 2)
+                .All(p => !IsPositionUnderAttack(chessboard, p, king.Side));
+        }
+        private List<Position> HorizontalPositionsBetweenPieces(IPiece piece1, IPiece piece2)
+        {
+            var y = piece1.Position.Y;
+            var x1 = piece1.Position.X;
+            var x2 = piece2.Position.X;
+            int maxX, minX;
+
+            if (x1 > x2)
+            {
+                maxX = x1;
+                minX = x2;
+            }
+            else
+            {
+                minX = x1;
+                maxX = x2;
+            }
+            return Enumerable.Range(minX + 1, maxX)
+                .Select(x => Position.CreateNumericPosition(x, y))
+                .ToList();
         }
     }
 }
