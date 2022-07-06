8564d8b618bc32f5cf4abcbdd175505a0c355ed1
Andrea Zavatta <zavattaandrea@gmail.com>
2022-07-06 09:48:18 +0200

    creating interface for handle null object value

---
+++ OOP21-Chess-csharp/AndreaZavatta/ICheckNull.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_Chess_csharp.AndreaZavatta
+{
+    public interface ICheckNull
+    {
+        public TResult IfNotNull<TInput, TResult>(TInput obj, Func<TInput, TResult> eval) where TResult : class where TInput : class
+        {
+            if (obj == null) return null;
+            return eval(obj);
+        }
+    }
+}
