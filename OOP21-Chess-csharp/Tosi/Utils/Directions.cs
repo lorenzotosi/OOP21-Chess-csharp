6e7bfb50410c9982824f0207e8bb90a0d29af7c6
Angela Speranza <angela.speranza@studio.unibo.it>
2022-07-08 11:28:01 +0200

    add the new pieces' directions

--- OOP21-Chess-csharp/Tosi/Utils/Directions.cs
+++ OOP21-Chess-csharp/Tosi/Utils/Directions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OOP21_Chess_csharp.Tosi.Utils
 {
@@ -34,6 +35,35 @@ namespace OOP21_Chess_csharp.Tosi.Utils
                 Position.CreateNumericPosition(-2, +1)
             };
             return positions;
+        }
+
+        public IEnumerable<Position> KingDir()
+        {
+            return RookDir().Concat(BishopDir());
+        }
+        
+        public IEnumerable<Position> RookDir()
+        {
+           var positions = new List<Position>()
+            {
+                Position.CreateNumericPosition(+1, 0),
+                Position.CreateNumericPosition(-1, 0),
+                Position.CreateNumericPosition(0, +1),
+                Position.CreateNumericPosition(0, -1)
+            };
+            return positions;
+        }
+        
+        public IEnumerable<Position> BishopDir()
+        {
+           var positions = new List<Position>()
+            {
+                Position.CreateNumericPosition(+1, +1),
+                Position.CreateNumericPosition(-1, -1),
+                Position.CreateNumericPosition(+1, -1),
+                Position.CreateNumericPosition(-1, +1)
+            };
+           return positions;
         }
     }
 }
